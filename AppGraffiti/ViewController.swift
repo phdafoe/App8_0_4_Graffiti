2728e5dbb8f27e28309e668287c1679f1e8e2f49
Andres <phdafoe@gmail.com>
2017-02-11 22:00:01 +0100

    Initial Commit

---
+++ AppGraffiti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppGraffiti
+//
+//  Created by User on 11/2/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
